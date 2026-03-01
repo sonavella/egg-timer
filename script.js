112676b59e07a65d2d327fa201e5dc57bf228573
sonavella <sonalsonal66565@gmail.com>
2026-03-01 22:56:56 +0530

    Fixed title bar layout for web browser and added spacing

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const { ipcRenderer } = require('electron');
+
 const minimizeBtn = document.getElementById('minimize-btn');
 const closeBtn = document.getElementById('close-btn');
 
-minimizeBtn.addEventListener('click', () => {
-    ipcRenderer.send('minimize-app');   
-});
+if (typeof require !== 'undefined') {
+   
+    const { ipcRenderer } = require('electron');
+    
+    minimizeBtn.addEventListener('click', () => {
+        ipcRenderer.send('minimize-app');   
+    });
 
-closeBtn.addEventListener('click', () => {
-    ipcRenderer.send('close-app');  
-});
+    closeBtn.addEventListener('click', () => {
+        ipcRenderer.send('close-app');  
+    });
+} else {
+    console.log("Running in Chrome! Buttons are in web-display mode.");
+    
+    minimizeBtn.addEventListener('click', () => {
+        alert("You are viewing the web version! Download the desktop app to minimize.");
+    });
+
+    closeBtn.addEventListener('click', () => {
+        alert("You are viewing the web version! Close this browser tab to exit.");
+    });
+}
+
 
 const startScreen = document.getElementById('start-screen');
 const selectionScreen = document.getElementById('selection-screen'); 
@@ -20,12 +36,9 @@ const timerDisplay = document.getElementById('time-display');
 const softEggButton = document.getElementById('soft-egg-btn');
 const poachedEggButton = document.getElementById('poached-egg-btn');
 
-
 let countdownInterval;
 
-
 function showScreen(screenToShow) {
-  
     startScreen.classList.remove('active');
     selectionScreen.classList.remove('active');
     timerScreen.classList.remove('active');
@@ -37,21 +50,17 @@ startBtn.addEventListener('click', () => {
     showScreen(selectionScreen);
 });
 
-
 function startTimer(totalSeconds) {
     showScreen(timerScreen);
     clearInterval(countdownInterval); 
     
     let remainingSeconds = totalSeconds;
     
-    
     updateDisplay(remainingSeconds);
-
     
     countdownInterval = setInterval(() => {
         remainingSeconds--; 
         updateDisplay(remainingSeconds); 
-
         
         if (remainingSeconds <= 0) {
             clearInterval(countdownInterval); 
@@ -61,19 +70,15 @@ function startTimer(totalSeconds) {
     }, 1000); 
 }
 
-
 function updateDisplay(totalSeconds) {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
     
-
     const formattedMinutes = minutes.toString().padStart(2, '0');
     const formattedSeconds = seconds.toString().padStart(2, '0');
     
     timerDisplay.textContent = `${formattedMinutes}:${formattedSeconds}`;
 }
-
-
 
 if (softEggButton) {
     softEggButton.addEventListener('click', () => {
@@ -88,6 +93,4 @@ if (poachedEggButton) {
         startTimer(time);
     });
 }
-
-
 
